375678aea0241132421e7b1c41a576431bd9aa65
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-02-14 19:16:24 +0600

    task 5 : complete

--- src/useWindowScroll.jsx
+++ src/useWindowScroll.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
+import { useWindowEvent } from './useWindowEvent'
 
 export function useWindowScroll() {
-	return <div></div>
+	const [scroll, setScroll] = useState({ x: 0, y: 0 })
+
+	useWindowEvent('scroll', () => {
+		setScroll({ x: window.scrollX, y: window.scrollY })
+	})
+
+	const scrollTo = ({ x, y }) => {
+		window.scrollTo(x, y)
+	}
+
+	return [scroll, scrollTo]
 }
